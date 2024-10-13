37d2bd6530294c9c6c9438dff845d2efd25d435b
Atichie <atichie525@gmail.com>
2024-10-13 19:35:34 +0300

    Add all changes

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'library',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -75,7 +76,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework.authenticatication.TokenAuthenticatication',
+            'rest_framework.authentication.TokenAuthentication',
         ],
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.IsAuthenticated',
